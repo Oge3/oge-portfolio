0621dbb5e3f81cf20e306e4fa462e4f9dee5b6c9
Oge3 <aguwambaogechi123@gmail.com>
2024-12-03 14:58:22 -0800

    index.html

---
+++ oge-portfolio/src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>WELCOME TO OGE'S PORTFOLIO</h1>
+      <p><h3>About Me</h3>
+I am Ogechi Aguwamba, a motivated and detail-oriented professional with a passion for excellence and innovation in problem-solving. 
+With a solid educational background and hands-on experience in roles requiring technical expertise and client engagement, I thrive on creating solutions that add value to individuals and organizations.
+My work is guided by a desire to make an impact, continually learn, and deliver meaningful results.</p>
+    </>
+  )
+}
+
+export default App
